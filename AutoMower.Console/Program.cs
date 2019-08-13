73fddb457806bdee880597696ad0db893aa094df
Etienne <etienne.pierrot+github@gmail.com>
2019-08-13 22:38:18 +0200

    add console application, doc, and example

---
+++ AutoMower.Console/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+
+namespace AutoMower.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = args[0];
+            string content = File.ReadAllText(path);
+
+            System.Console.WriteLine("input file ");
+            System.Console.WriteLine(content);
+
+            string result = new Domain.AutoMower().Run(content);
+
+            System.Console.WriteLine("result ");
+            System.Console.WriteLine(result);
+
+            System.Console.WriteLine("Press a key to exit.");
+            System.Console.ReadKey();
+        }
+    }
+}
